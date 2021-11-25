dd5c6bee235a8033710fd94d7e24cf0b1c59e62f
Sohini Makar <sohini.makar@gmail.com>
2021-11-25 11:34:31 -0800

    Balanced parentheses check

--- problems/isValid.js
+++ problems/isValid.js
@@ -11,6 +11,7 @@ var isValid = function(s) {
     let stack =[];
     
     for (let i = 0; i < s.length; i++) {
+        
         if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
             if (stack.length == 0) {
                 return false;
