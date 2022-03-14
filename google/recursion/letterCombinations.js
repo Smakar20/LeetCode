a56b351d4e9ffc8f735d936f12c5acd177dee6ec
Sohini Makar <sohini.makar@gmail.com>
2022-03-13 21:07:37 -0700

    Letter Combinations of a Phone Number

---
+++ google/recursion/letterCombinations.js
@@ -0,0 +1,52 @@
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = ""
+Output: []
+
+Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+Constraints:
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+*/
+// Time: O(4^n * n) | Space: O(4^n * n) -> where n is the length of the phone number
+const digitMap = {
+        2: ['a', 'b', 'c'],
+        3: ['d','e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m',  'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    };
+var letterCombinations = function(digits) {
+    let output = [];
+    let current = [];
+    letterCombinationsUtil(0, digits, output, current);
+    return output;
+};
+
+function letterCombinationsUtil(idx, digits, output, current) {
+    if (idx === digits.length) {
+        if(current.length > 0) output.push(current.join(''));
+        return;
+    }
+    
+    const curNum = digits[idx];
+    for (let digit of digitMap[curNum]) {
+        current.push(digit);
+        letterCombinationsUtil(idx + 1, digits, output, current);
+        current.pop(digit);
+    }
+}
