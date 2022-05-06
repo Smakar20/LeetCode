2718f639611ddbb093fc1d7f3ae659683c0215ad
Sohini Makar <sohini.makar@gmail.com>
2022-05-06 15:36:55 -0700

    longest substring without repeating characters

--- google/Arrays&Strings/lengthOfLongestSubstring.js
+++ google/Arrays&Strings/lengthOfLongestSubstring.js
@@ -2,19 +2,23 @@
 Given a string s, find the length of the longest substring without repeating characters.
 
 Example 1:
-
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
-Example 2:
 
+Example 2:
 Input: s = "bbbbb"
 Output: 1
 Explanation: The answer is "b", with the length of 1.
-Example 3:
 
+Example 3:
 Input: s = "pwwkew"
 Output: 3
+
+Example 4:
+Input: s = "abba"
+output: 2
+
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  
@@ -24,18 +28,15 @@ Constraints:
 s consists of English letters, digits, symbols and spaces.
 */
 
-//Time: O(n) | Space: O(min(n, a)
+//Time: O(n) | Space: O(min(n, a) -> n is the length of the input string and a is the number of unique characters
 var lengthOfLongestSubstring = function(s) {
-    let [longestSubStrPos, curSubStrLen, startIdx] = [[0,0], 0, 0];
-    let charMap = {};
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] in charMap) {
-            startIdx = Math.max(startIdx, charMap[s[i]] + 1);
+    let [charMap, longest, l] = [{}, 0, 0];
+    for (let r = 0; r < s.length; r++) {
+        if (s[r] in charMap && charMap[s[r]] >= l) {
+            l = charMap[s[r]] + 1; 
         }
-        
-        curSubStrLen = i + 1 - startIdx;
-        if (longestSubStrPos[1] - longestSubStrPos[0] < curSubStrLen) longestSubStrPos = [startIdx, i + 1];
-        charMap[s[i]] = i;
+        charMap[s[r]] = r;
+        longest = Math.max(longest, r + 1 - l);
     }
-    return longestSubStrPos[1] - longestSubStrPos[0];
+    return longest;
 };
