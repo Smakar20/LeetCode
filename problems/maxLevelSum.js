60ea9071c57a61be9a7e48e3494e2242e9babea3
Sohini Makar <sohini.makar@gmail.com>
2023-09-28 12:50:34 -0700

    Maximum Level Sum of a Binary Tree

---
+++ problems/maxLevelSum.js
@@ -0,0 +1,62 @@
+/*
+Maximum Level Sum of a Binary Tree
+
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+
+Example 1:
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+
+Example 2:
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-10^5 <= Node.val <= 10^5
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+    const queue = [root, null];
+    let [maxSum, curSum, curLevel, ansLevel] = [-Infinity, 0, 1, 1];
+
+    while (queue.length > 0) {
+        let curr = queue.shift();
+        if (curr === null){
+            if (queue.length > 0) queue.push(null);
+            if (curSum > maxSum) {
+                maxSum = curSum;
+                ansLevel = curLevel;
+            }
+            curLevel++;
+            curSum = 0;
+            continue;
+        }
+
+        curSum += curr.val;
+
+        if(!!curr.left) queue.push(curr.left);
+        if(!!curr.right) queue.push(curr.right);
+    }
+    return ansLevel;
+};
