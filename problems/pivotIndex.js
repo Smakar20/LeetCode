d8cbf13f986b614c4cd6d157976f1603a17b62a3
Sohini Makar <sohini.makar@gmail.com>
2023-09-28 23:17:33 -0700

    Find Pivot Index

--- problems/pivotIndex.js
+++ problems/pivotIndex.js
@@ -1,4 +1,5 @@
 /*
+Find Pivot Index
 Given an array of integers nums, calculate the pivot index of this array.
 
 The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
@@ -36,12 +37,10 @@ Constraints:
 */
 //Time: O(n) | Space: O(1)
 var pivotIndex = function(nums) {
-    let sum = nums.reduce((acc,num) => acc + num, 0);
-    let leftSum = 0;
-    for(let i = 0; i < nums.length; i++) {
-        if(leftSum == sum - leftSum - nums[i]) {
-            return i;
-        }
+    let [leftSum, rightSum] =[0, nums.reduce((acc, num) => acc += num, 0)];
+    for (let i = 0; i < nums.length; i++) {
+        rightSum -= nums[i];
+        if (leftSum === rightSum) return i;
         leftSum += nums[i];
     }
     return -1;
