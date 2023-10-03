dca2371e4a23f758f0841618d86479e8781def72
Sohini Makar <sohini.makar@gmail.com>
2023-10-02 21:16:34 -0700

    Decode String

--- problems/decodeString.js
+++ problems/decodeString.js
@@ -62,4 +62,30 @@ var decodeString = function(s) {
     }
 
     return stack.join('');
+ 
+ //another way
+ /*
+ let stack = [];
+    let currStr = '';
+    let currNum = 0;
+    
+    for (let i = 0; i < s.length; i ++) {
+        if (s[i] === '[') {
+            stack.push(currStr);
+            stack.push(currNum);
+            currStr = '';
+            currNum = 0;
+        } else if (s[i] === ']') {
+            let prevNum = stack.pop();
+            let prevStr = stack.pop();
+            currStr = prevStr + currStr.repeat(prevNum);
+        } else if (s[i].match(/[0-9]/)) {
+            currNum = currNum * 10 + Number(s[i]);
+        } else {
+            currStr += s[i];
+        }
+    }
+    
+    return currStr;
+ */
 };
