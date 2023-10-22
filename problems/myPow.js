6c1be711763bfef2a789a418b67520a28fb29d52
Sohini Makar <sohini.makar@gmail.com>
2023-10-21 20:23:47 -0700

    POw(x, n)

--- problems/myPow.js
+++ problems/myPow.js
@@ -1,5 +1,5 @@
 /*
-Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
 
 Example 1:
 Input: x = 2.00000, n = 10
@@ -21,8 +21,45 @@ Constraints:
 */
 
 //Time: O(logn) | Space: O(1)
+/*
+Pow(x, n)
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example 2:
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+Example 3:
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+Constraints:
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-10^4 <= xn <= 10^4
+*/
+
 var myPow = function(x, n) {
     if (n === 0) return 1;
-    if(n < 0) return myPow(1/x, -n);
-    return (n % 2 === 0) ? myPow(x*x, n/2) : x* myPow(x*x, Math.floor(n/2)); 
+
+    if (n < 0) {
+        x = 1 / x;
+        n = -n;
+    }
+
+    if (n % 2 === 0) {
+        let halfPow = myPow(x, n / 2);
+        return halfPow * halfPow;
+    }
+
+    return x * myPow(x, n - 1);
 };
+
